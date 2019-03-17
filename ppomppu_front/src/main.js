739e42bac895ec49809119d8d6abe72ddbe89b65
Kim Do Ky <makingfunk0@gmail.com>
2019-03-17 17:52:13 +0900

    basic router

--- ppomppu_front/src/main.js
+++ ppomppu_front/src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = { template: '<div>Login Page</div>' }
+const UserInfo = { template: '<div>User Info Page</div>' }
+const ContentList = { template: '<div>Content List</div>' }
+const NotFound = { template: '<div>Page not found</div>' }
+
+const routes = {
+  '/': App,
+  '/login': Login,
+  '/userinfo': UserInfo,
+  '/list': ContentList
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] || NotFound
+    }
+  },
+  render(h) {
+    return h(this.VueComponent)
+  }
 })
