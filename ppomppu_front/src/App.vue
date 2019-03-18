a45e953ada4c93a944c9511d1b45868960bdef4b
Kim Do Ky <makingfunk0@gmail.com>
2019-03-18 23:33:37 +0900

    router link

--- ppomppu_front/src/App.vue
+++ ppomppu_front/src/App.vue
@@ -3,14 +3,18 @@
     <img src="./assets/logo.png">
     <div>
       code scaffolding
+      <Navbar />
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
   name: 'app',
+  components: { Navbar }
 }
 </script>
 
---
+++ ppomppu_front/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">login</router-link>
+    <router-link to="/UserInfo">UserInfo</router-link>
+    <router-link to="/list">Contentlist</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
