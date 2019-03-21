771714b2a549313801c48cec5465a1bc62f0cf2f
Kim Do Ky <makingfunk0@gmail.com>
2019-03-22 00:32:15 +0900

    login form

--- ppomppu_front/src/components/Login.vue
+++ ppomppu_front/src/components/Login.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
-    Login Page
-  </div>
+    <h2>Log in</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="email">
+        <label for="email">Email</label>
+        <input class="form-control" type="text" v-model="email" autofocus placeholder="test@test.com" />
+      </div>
+      <div class="password">
+        <label for="password">Password</label>
+        <input class="form-control" type="password" v-model="password" />
+      </div>
+      <button type="submit" class="btn" :class="{'btn-success': !invalidForm}" :disabled="invalidForm">Login</button>
+      <p class="error" v-if="error">{{error}}</p>
+    </form>
+    </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  computed: {
+    invalidForm() {
+      return !this.email || !this.password
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.email, this.password)
+    }
+  }
 }
 </script>
 
 <style>
+.login {
+  width: 400px;
+  margin: 0 auto;
+}
+.error {
+  color: #f00;
+}
 </style>
