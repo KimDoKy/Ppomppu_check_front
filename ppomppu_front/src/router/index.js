8b58217d1c684c3fd1479cbb762b42b9190d8880
Kim Do Ky <makingfunk0@gmail.com>
2019-03-21 23:55:20 +0900

    Navigation Guards

--- ppomppu_front/src/router/index.js
+++ ppomppu_front/src/router/index.js
@@ -8,13 +8,19 @@ import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login },
-    { path: '/userinfo', component: UserInfo },
-    { path: '/list', component: ContentList },
+    { path: '/userinfo', component: UserInfo, beforeEnter: requireAuth },
+    { path: '/list', component: ContentList, beforeEnter: requireAuth },
     { path: '*', component: NotFound }
   ]
 })
