928b67e272be196812406dbdb5a6c5598f2595dd
Kim Do Ky <makingfunk0@gmail.com>
2019-03-22 13:19:11 +0900

    logout

--- ppomppu_front/src/components/Navbar.vue
+++ ppomppu_front/src/components/Navbar.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <router-link to="/">Home</router-link>
-    <router-link to="/login">login</router-link>
+    <a href="" v-if="isAuth" @click.prevent="logout">Logout</a>
+    <router-link v-else to="/login">Login</router-link>
     <router-link to="/UserInfo">UserInfo</router-link>
     <router-link to="/list">Contentlist</router-link>
   </div>
 </template>
 
 <script>
+import {setAuthInHeader} from '../api'
+
 export default {
+  computed: {
+    isAuth() {
+      return !!localStorage.getItem('token')
+    }
+  },
+  methods: {
+    logout() {
+      delete localStorage.token
+      setAuthInHeader(null)
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
