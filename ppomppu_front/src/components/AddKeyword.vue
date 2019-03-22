e24be4d218d3d2967bfdd00d08c53cb791610829
Kim Do Ky <makingfunk0@gmail.com>
2019-03-22 20:13:51 +0900

    modal - add keyword

---
+++ ppomppu_front/src/components/AddKeyword.vue
@@ -0,0 +1,59 @@
+<template>
+  <Modal>
+    <div slot="header">
+      <h2>
+        Add New Keyword
+        <a href="" class="modal-default-button"
+          @click.prevent="close">&times;</a>
+      </h2>
+    </div>
+    <div slot="body">
+      <form id="add-keyword-form"
+        @submit.prevent="addKeyword">
+        <input type="text" v-model="input" ref="input">
+      </form>
+    </div>
+    <div slot="footer">
+      <button type="submit" class="btn" :class="{'btn-success': valid}"
+        form="add-keyword-form" :disabled="!valid">
+        Add Keyword
+      </button>
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      valid: false,
+      input: ''
+    }
+  },
+  watch: {
+    input(v) {
+      this.valid = v.trim().length > 0
+    }
+  },
+  mounted() {
+    this.$refs.input.focus()
+  },
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+    addKeyword() {
+      this.$emit('close')
+      this.$emit('submit', this.input)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- ppomppu_front/src/components/Home.vue
+++ ppomppu_front/src/components/Home.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    Home
+    <div class="add-keyword">
+      <a href="" @click.prevent="addKeyword">
+        Add keyword
+      </a>
+    </div>
+    <AddKeyword v-if="isAddKey" @close="isAddKey=false"/>
   </div>
 </template>
 
 <script>
-export default {
+import AddKeyword from './AddKeyword.vue'
 
+export default {
+  data() {
+    return {
+      isAddKey : false,
+    }
+  },
+  components: {
+    AddKeyword
+  },
+  methods: {
+    addKeyword() {
+      this.isAddKey = true
+    }
+  }
 }
 </script>
 
