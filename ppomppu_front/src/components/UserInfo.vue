792782b0d6537ce6e551b949887ef8f509040c15
Kim Do Ky <makingfunk0@gmail.com>
2019-03-28 21:42:17 +0900

    add userInfo

--- ppomppu_front/src/components/UserInfo.vue
+++ ppomppu_front/src/components/UserInfo.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
-    User Info
+    <h2>User Info</h2>
+    <table>
+      <tr>
+        <th>username</th>
+        <th>email</th>
+      </tr>
+      <tr>
+        <td>{{userData.username}}</td>
+        <td>{{userData.email}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import {auth} from '../api'
+
 export default {
+  data() {
+    return {
+      userData: {}
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      auth.userInfo()
+        .then(result => this.userData = result)
+    }
+  }
 }
 </script>
 
 <style>
+.table {
+  border: 1px solid;
+}
 </style>
