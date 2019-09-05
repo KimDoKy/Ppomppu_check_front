7c6ee04108e4c2e2d463a88bb5455fbd7dc9696d
Kim Do Ky <makingfunk0@gmail.com>
2019-09-05 22:29:46 +0900

    날짜 데이터 포맷 변경

--- ppomppu-checker/src/main.js
+++ ppomppu-checker/src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
+import moment from 'moment'
 
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('YY/MM/DD')
+  }
+})
 new Vue({
   el: '#app',
   router,
